a941853fc22ce028a30de254c751320750e5a4c0
Max Iniguez <maximillini86@gmail.com>
2016-02-18 14:45:33 -0600

    Update game_master.rb

--- app/models/game_master.rb
+++ app/models/game_master.rb
@@ -3,7 +3,7 @@ class GameMaster < ActiveRecord::Base
   has_many :parties, through: :games
   has_many :players, through: :parties
 
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :password_digest, presence: true
   validates :username, :email, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, message: "Must be valid email address" }
 
